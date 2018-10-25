d0a19bf75578cc00ecb9ebbc0cc2a320e031a220
rukok <rukokuro@gmail.com>
2018-10-25 13:25:50 +0200

    Start Posts Feature Part 9 - Complete

--- app/src/main/java/com/example/artem/photoblogtvaclesson/NewPostActivity.java
+++ app/src/main/java/com/example/artem/photoblogtvaclesson/NewPostActivity.java
@@ -1,13 +1,51 @@
 package com.example.artem.photoblogtvaclesson;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewPostActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private ImageView newPostImage;
+    private EditText newPostDesc;
+    private Button newPostBtn;
+    private ProgressBar progressBar;
+
+    private Uri postImageUri;
+
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private String current_user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +53,113 @@ public class NewPostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_post);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        newPostImage = (ImageView) findViewById(R.id.new_post_image);
+        newPostDesc = (EditText) findViewById(R.id.new_post_desc);
+        newPostBtn = (Button) findViewById(R.id.new_post_btn);
+        progressBar = (ProgressBar) findViewById(R.id.new_post_progress);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        current_user_id = firebaseAuth.getCurrentUser().getUid();
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("New Post");
+
+        newPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setMinCropResultSize(512, 512)
+                        .setAspectRatio(1, 1)
+                        .start(NewPostActivity.this);
+            }
+        });
+
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String description = newPostDesc.getText().toString();
+                if (!TextUtils.isEmpty(description) && postImageUri != null) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    String randomImageName = FieldValue.serverTimestamp().toString();
+                    final StorageReference file_path = storageReference.child("post_images").child(randomImageName + ".jpg");
+                    file_path.putFile(postImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if (task.isSuccessful()) {
+
+                                file_path.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String download_uri = String.valueOf(uri);
+                                        Map<String, Object> postMap = new HashMap<>();
+                                        postMap.put("image_url", download_uri);
+                                        postMap.put("desc", description);
+                                        postMap.put("user_id", current_user_id);
+                                        postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                        firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                                if (task.isSuccessful()){
+                                                    Toast.makeText(NewPostActivity.this, "Successful Post", Toast.LENGTH_SHORT).show();
+                                                    Intent mainIntent = new Intent(NewPostActivity.this, MainActivity.class);
+                                                    startActivity(mainIntent);
+                                                    finish();
+                                                }else {
+
+
+                                                }
+                                                progressBar.setVisibility(View.INVISIBLE);
+                                            }
+                                        });
+                                    }
+                                });
+
+
+                            } else {
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    postImageUri = result.getUri();
+                    newPostImage.setImageURI(postImageUri);
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Exception error = result.getError();
+                }
+            }
         }
-    }
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                    return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
 }
